33e06fa12e084cc0cd2ef5c78c267e2b71806899
Ashley <xxdarkslayerxx@googlemail.com>
2026-03-18 23:15:44 +0100

    Comma fix. I hate commas.

--- Arrowgene.Ddon.Database/Files/Database/Script/schema_sqlite.sql
+++ Arrowgene.Ddon.Database/Files/Database/Script/schema_sqlite.sql
@@ -866,7 +866,7 @@ CREATE TABLE IF NOT EXISTS "ddon_partner_pawn_pending_rewards"
     "pawn_id"      INTEGER NOT NULL,
     "reward_level" INTEGER NOT NULL,
     CONSTRAINT "pk_ddon_partner_pawn_pending_rewards" PRIMARY KEY ("character_id", "pawn_id", "reward_level"),
-    CONSTRAINT "fk_ddon_partner_pawn_pending_rewards_character_id" FOREIGN KEY ("character_id") REFERENCES "ddon_character" ("character_id") ON DELETE CASCADE
+    CONSTRAINT "fk_ddon_partner_pawn_pending_rewards_character_id" FOREIGN KEY ("character_id") REFERENCES "ddon_character" ("character_id") ON DELETE CASCADE,
     CONSTRAINT "fk_ddon_partner_pawn_pending_rewards_pawn_id" FOREIGN KEY ("pawn_id") REFERENCES "ddon_pawn" ("pawn_id") ON DELETE CASCADE
 );
 
