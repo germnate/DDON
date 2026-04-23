d3d0ff30a2a3b178aa386542063b94dd6c2f2246
Paul Campbell <pacampbell87@gmail.com>
2026-04-23 08:46:45 -0400

    fix: Add a server config for IANA ID
    Some timers are running off local server time, while others are using
    UTC 0. Added a a new setting ServerTimeZone which uses a IANA ID. It is
    used by the task scheduler and and other calendar aware timing events.
    By default it is set to "TimeZoneId.Japan" (JST +9). A helper class named
    TimeZoneId.cs exists so a admin can use a named constant instead of magic numbers.

--- Arrowgene.Ddon.GameServer/Tasks/HourlyTask.cs
+++ Arrowgene.Ddon.GameServer/Tasks/HourlyTask.cs
@@ -5,20 +5,16 @@ namespace Arrowgene.Ddon.GameServer.Tasks
 {
     public abstract class HourlyTask : SchedulerTask
     {
-        /// <summary>
-        /// Task which is always scheduled to run on the hour in the timezone of the server currently running.
-        /// </summary>
-        /// <param name="type">The task type associated with this task</param>
         public HourlyTask(TaskType type) : base(ScheduleInterval.Hourly, type)
         {
         }
 
         public override long NextTimestamp()
         {
-            var now = DateTime.Now;
+            var now = DateTimeOffset.UtcNow.ToOffset(Offset);
             var next = now.AddHours(1);
-            var nextTime = new DateTime(next.Year, next.Month, next.Day, next.Hour, 0, 0);
-            return new DateTimeOffset(now.Add(nextTime - now)).ToUnixTimeSeconds();
+            var nextTime = new DateTimeOffset(next.Year, next.Month, next.Day, next.Hour, 0, 0, Offset);
+            return nextTime.ToUnixTimeSeconds();
         }
 
         public override string TaskTypeName()
