f2abd532b0ac1d9fe3847d5664a8ab8ef0cf1d9f
Bustamante, Nathan <NBustamante@finys.com>
2026-09-16 13:15:01 -0400

    update bazaar cron

--- scripts/bazaar-cron.sh
+++ scripts/bazaar-cron.sh
@@ -4,10 +4,29 @@ set -eu
 : "${BAZAAR_ROTATION_SCHEDULE:=0 */6 * * *}"
 
 cat >/etc/cron.d/ddon-bazaar <<EOF
-${BAZAAR_ROTATION_SCHEDULE} root DDON_DISABLE_ASSET_WATCHERS=true /var/ddon/server/Arrowgene.Ddon.Cli bazaar rotate --config=Files/Arrowgene.Ddon.config.json >> /var/log/ddon-bazaar.log 2>&1
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+TZ=${TZ:-America/New_York}
+DB_TYPE=${DB_TYPE:-}
+DB_DATABASE=${DB_DATABASE:-}
+DB_FOLDER=${DB_FOLDER:-}
+DB_HOST=${DB_HOST:-}
+DB_USER=${DB_USER:-}
+DB_PASS=${DB_PASS:-}
+DB_PORT=${DB_PORT:-}
+DB_WIPE_ON_STARTUP=${DB_WIPE_ON_STARTUP:-}
+DB_BUFFER_SIZE=${DB_BUFFER_SIZE:-}
+DB_NO_RESET_ON_CLOSE=${DB_NO_RESET_ON_CLOSE:-}
+DB_ENABLE_TRACING=${DB_ENABLE_TRACING:-}
+DB_ENABLE_POOLING=${DB_ENABLE_POOLING:-}
+DB_MAX_AUTO_PREPARE=${DB_MAX_AUTO_PREPARE:-}
+
+${BAZAAR_ROTATION_SCHEDULE} root cd /var/ddon/server && DDON_DISABLE_ASSET_WATCHERS=true /var/ddon/server/Arrowgene.Ddon.Cli bazaar rotate --config=/var/ddon/server/Files/Arrowgene.Ddon.config.json >> /proc/1/fd/1 2>&1
 EOF
 
 chmod 0644 /etc/cron.d/ddon-bazaar
-touch /var/log/ddon-bazaar.log
+
+echo "Installed DDON bazaar cron schedule: ${BAZAAR_ROTATION_SCHEDULE}"
+echo "Cron job output will be written to the container log."
 
 exec cron -f
